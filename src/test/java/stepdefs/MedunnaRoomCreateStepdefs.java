4396940ecd65f3ad54541d16bafe50c19aafbe68
isminiz <115974069+remziyebuer@users.noreply.github.com>
2023-07-11 23:10:33 +0200

    Initial commit

---
+++ src/test/java/stepdefs/MedunnaRoomCreateStepdefs.java
@@ -0,0 +1,47 @@
+package stepdefs;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.When;
+
+public class MedunnaRoomCreateStepdefs {
+
+    @When("Click on ItemsAndTitles")
+    public void clickOnItemsAndTitles() {
+    }
+
+    @And("click on Room option")
+    public void clickOnRoomOption() {
+    }
+
+    @And("click on Create a new room button")
+    public void clickOnCreateANewRoomButton() {
+    }
+
+    @And("enter {string} room number input")
+    public void enterRoomNumberInput(String arg0, String arg1) {
+    }
+
+    @And("select Suite option from Room Type dropdown")
+    public void selectSuiteOptionFromRoomTypeDropdown() {
+    }
+
+    @And("click on Status checkbox")
+    public void clickOnStatusCheckbox() {
+    }
+
+    @And("enter {string} in Price input")
+    public void enterInPriceInput(String arg0, String arg1) {
+    }
+
+    @And("enter {string} in Description input")
+    public void enterInDescriptionInput(String arg0, String arg1) {
+    }
+
+    @And("click on Save button")
+    public void clickOnSaveButton() {
+    }
+
+    @And("close the application")
+    public void closeTheApplication() {
+    }
+}
---
+++ src/test/java/stepdefs/MedunnaSignInStepdefs.java
@@ -0,0 +1,33 @@
+package stepdefs;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+
+public class MedunnaSignInStepdefs {
+    @Given("Go to {string}")
+    public void goTo(String arg0) {
+    }
+
+    @When("click on user icon")
+    public void clickOnUserIcon() {
+    }
+
+    @And("click on sign in option")
+    public void clickOnSignInOption() {
+    }
+
+    @And("enter username")
+    public void enterUsername() {
+    }
+
+    @And("enter password")
+    public void enterPassword() {
+    }
+
+    @And("click on sign in submit  button")
+    public void clickOnSignInSubmitButton() {
+    }
+
+
+}
