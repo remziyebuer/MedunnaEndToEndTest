b2b19b4ae091daee708da9639916ad1d261f3d7a
isminiz <115974069+remziyebuer@users.noreply.github.com>
2023-07-13 23:49:40 +0200

    ...

---
+++ src/test/java/base_url/MedunnaBaseUrl.java
@@ -0,0 +1,19 @@
+package base_url;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+import static utilities.AuthenticationMedunna.generateToken;
+
+public class MedunnaBaseUrl {
+
+    public static RequestSpecification spec;
+
+    public static void setUp(){
+
+        spec = new RequestSpecBuilder().setContentType(ContentType.JSON)
+                .addHeader("Authorization","Bearer "+ generateToken())
+                .setBaseUri("https://medunna.com").build();
+    }
+
+}
